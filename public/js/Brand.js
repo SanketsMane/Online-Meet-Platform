28841d3157fa5a560b499bdaae261a8ff1010e79
Sanket Mane <sanketpatil515151@gmail.com>
2026-02-12 11:03:58 +0530

    Polish UI: typography, spacing, and component refinement

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -49,7 +49,7 @@ let BRAND = {
         joinLastLabel: 'Your recent room:',
     },
     site: {
-        title: 'tawktoo, Free Video Calls, Messaging and Screen Sharing',
+        title: 'tawktoo - Free Video Calls, Messaging and Screen Sharing',
         icon: '../images/logo.svg',
         appleTouchIcon: '../images/logo.svg',
         newRoomTitle: 'Pick name. <br />Share URL. <br />Start conference.',
@@ -60,7 +60,7 @@ let BRAND = {
         description:
             'tawktoo powered by WebRTC and mediasoup, Real-time Simple Secure Fast video calls, messaging and screen sharing capabilities in the browser.',
         keywords:
-            'webrtc, miro, mediasoup, mediasoup-client, self hosted, voip, sip, real-time communications, chat, messaging, meet, webrtc stun, webrtc turn, webrtc p2p, webrtc sfu, video meeting, video chat, video conference, multi video chat, multi video conference, peer to peer, p2p, sfu, rtc, alternative to, zoom, microsoft teams, google meet, jitsi, meeting',
+            'webrtc, miro, mediasoup, mediasoup-client, self hosted, voip, sip, real-time communications, chat, messaging, meet, webrtc stun, webrtc turn, webrtc p2p, webrtc sfu, video meeting, video chat, video conference, multi video chat, multi video conference, peer to peer, p2p, sfu, rtc, alternative to, zoom, microsoft teams, jitsi, meeting',
     },
     html: {
         topSponsors: true,
