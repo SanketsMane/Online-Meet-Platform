9c9cc45f25b025428d5ad6cd41de7887503b9bec
Sanket Mane <sanketpatil515151@gmail.com>
2026-03-07 13:57:12 +0530

    Fixed CMS navigation, added contact shortcut, and adjusted DB models for SQLite compatibility - Sanket

--- app/src/db/models.js
+++ app/src/db/models.js
@@ -18,7 +18,7 @@ const Tenant = sequelize.define('Tenant', {
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        unique: false,
         validate: {
             isEmail: true,
         },
@@ -194,7 +194,7 @@ const User = sequelize.define('User', {
     email: {
         type: DataTypes.STRING,
         allowNull: true,
-        unique: true,
+        unique: false,
         validate: {
             isEmail: true,
         },
